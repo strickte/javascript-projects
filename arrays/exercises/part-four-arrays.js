699b0dccad013bd1f3e925031d4fe6890b58daf9
strickte <166240135+strickte@users.noreply.github.com>
2024-05-07 21:24:58 -0500

    Arrays Part 4

--- arrays/exercises/part-four-arrays.js
+++ arrays/exercises/part-four-arrays.js
@@ -1,10 +1,22 @@
-let holdCabinet1 = ['duct tape', 'gum', 3.14, false, 6.022e23];
-let holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
+const holdCabinet1 = ['duct tape', 'gum', 3.14, false, 6.022e23];
+const holdCabinet2 = ['orange drink', 'nerf toys', 'camera', 42, 'parsnip'];
 
 //Explore the methods concat, slice, reverse, and sort to determine which ones alter the original array.
 
 //1) Print the result of using concat on the two arrays. Does concat alter the original arrays? Verify this by printing holdCabinet1 after using the method.
+console.log(holdCabinet1.concat(holdCabinet2));
+console.log(holdCabinet1);
 
 //2) Print a slice of two elements from each array. Does slice alter the original arrays?
+console.log(holdCabinet1.slice(0,3));
+console.log(holdCabinet2.slice(2,4));
+console.log(holdCabinet1);
+console.log(holdCabinet2);
 
 //3) reverse the first array, and sort the second. What is the difference between these two methods? Do the methods alter the original arrays?
+holdCabinet1.reverse()
+holdCabinet2.sort()
+console.log(holdCabinet1);
+console.log(holdCabinet2);
+//The reverse method acts as the name suggests, reversing the order of the array.
+//The sort method sorts numerically (though not by value per se), then upper, then lower case alphabetically. 
