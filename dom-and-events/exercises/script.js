5a8917c5484377b40e4fb73e3b26dbd9483e30b0
strickte <166240135+strickte@users.noreply.github.com>
2024-08-13 12:59:20 -0500

    Chap 23 exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -1,10 +1,26 @@
-function init () {
-    const missionAbort = document.getElementById("abortMission");
-    const button = document.getElementById("liftoffButton");
-    const paragraph = document.getElementById("statusReport");
+function init() {
+  const missionAbort = document.getElementById("abortMission");
+  const button = document.getElementById("liftoffButton");
+  const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+  // Put your code for the exercises here.
+
+  button.addEventListener("click", () => {
+    paragraph.innerHTML = "Houston, we have liftoff!";
+  });
+  missionAbort.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "red";
+  });
+  missionAbort.addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor = "";
+  });
+  missionAbort.addEventListener("click", () => {
+    if (
+      window.confirm("Are you sure you want to abort the mission?") === true
+    ) {
+      paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+    }
+  });
 }
 
 window.addEventListener("load", init);
